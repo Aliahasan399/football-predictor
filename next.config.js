13c85e4334cf76dfa9f84e23e211bc0f1a7f69d6
Hermes Deploy <hermes@nousresearch.com>
2026-06-15 22:17:53 +0600

    Upgrade Next.js + Cloudflare Pages config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 import createNextIntlPlugin from 'next-intl/plugin';
+import { withCloudflare } from '@cloudflare/next-on-pages';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@vercel/analytics', '@vercel/speed-insights', 'lucide-react', 'recharts'],
 };
 
-export default createNextIntlPlugin('./i18n/request.js')(nextConfig);
+export default withCloudflare(createNextIntlPlugin('./i18n/request.js')(nextConfig));
